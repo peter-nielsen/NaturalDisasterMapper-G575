56f9da48aa5cd3ac1468c5cab14040d6fe1ffb25
JacobHrubecky <jhrubecky129@gmail.com>
2017-05-08 16:45:23 -0500

    Working on D3 graph

--- js/main2.js
+++ js/main2.js
@@ -20,6 +20,16 @@ var activeField;
 var attributes;
 var sauce;
 var mymap;
+//chart width and height
+var width = window.innerWidth * 0.15;
+var height = window.innerWidth * 0.15;
+//scales
+var x = d3.scaleLinear()
+    .range([0, width + 50])
+    .domain([2000, 2016]);
+var y = d3.scaleLinear()
+    .range([0, height-27])
+    .domain([600, 0]);
 
 // assigns the respected geojsons to the apropriate variables
 function getData(mymap) {
@@ -676,11 +686,7 @@ allLayers.stateWildfiresLayer = L.geoJson(state_eventsJSON, {
 		}
 	});
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> origin/master
 	activeField = "Total_Events";
 	// creating an array of attributes
 	attributes = processData(state_eventsJSON, activeField);
@@ -1339,28 +1345,12 @@ function Popup(properties, layer, radius){
 // create graph for the initial state view
 function stateGraph(csvData){
 
-    //print info for each item/state in csv file
-    for (var row = 0; row < csvData.length; row++){
-        console.log(csvData[row]);
-    }
-
-
-    //chart width and height
-    var width = window.innerWidth * 0.15;
-    var height = window.innerWidth * 0.15;
-
-
     //chart title
     var title = d3.select('#section-1')
-        .html('<br>Total Events By State</br>2000-2016')
+        .html('<br>' + activeField + 'By State</br>2000-2016')
         .style('font-family', 'Helvetica, sans-serif')
         .style('text-align', 'center')
         .style('font-weight', 'bold');
-
-
-    //chart width and height
-    var width = window.innerWidth * 0.15;
-    var height = window.innerWidth * 0.15;
 
     // svg to contain chart
     var vis = d3.select('#section-1')
@@ -1369,36 +1359,49 @@ function stateGraph(csvData){
         .attr('height', height)
         .attr("class", "chart");
 
-    //scales
-    var x = d3.scaleLinear()
-        .range([0, width + 50])
-        .domain([2000, 2016]);
-
-    var y = d3.scaleLinear()
-        .range([0, height-27])
-        .domain([600, 0]);
-
     //axis
     var xAxis = d3.axisBottom()
-				.tickFormat(d3.format("d"))
+        .tickFormat(d3.format("d"))
         .scale(x);
     var yAxis = d3.axisLeft()
         .scale(y);
 
     //Adding the scales to the chart. placement is tricky.
-    vis.append("svg:g")
+   vis.append("svg:g")
         .attr("transform", 'translate(27,' + (height-20).toString() + ')')
         .call(xAxis);
     vis.append("svg:g")
         .attr("transform", 'translate(27, 6)')
         .call(yAxis);
-    var lineArea = d3.select('.chart')
-        .append('svg')
-        .attr('width', width-27)
-        .attr('height', height-20)
-        .style('fill', 'black');
+    
+    //create label for tooltip
+    var tooltip = d3.select('#section-1')
+        .append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+    
+    var lines = vis.selectAll('.lines')
+        .attr('transform', 'translate(28,6)')
+        .append('polyline')
+        .on('mouseover', function(){
+            console.log(this.id);
+            d3.select('#' + (this.id) + '')
+                .style("stroke-width", "6");
+            d3.select('.tooltip')
+                .style("opacity", 1)
+                .html('<p>' + this.id + '</p>')
+                .style('left', width/2 +'px')
+                .style('top', '70px');
+        })
+        .on("mouseout", function(){
+            d3.select('#'+ (this.id).toString())
+                .style("stroke-width", "3.5");
+            d3.select('.tooltip')      
+                .style("opacity", 0);
+        });
 
 
+    /*
     for (var row = 0; row < csvData.length; row++){
         var lines = vis.append('polyline')
             .attr('points', (x(2000)).toString() + ',' + (y(csvData[row]['Total_Events_2000'])).toString() + ',' +
@@ -1447,15 +1450,10 @@ function stateGraph(csvData){
         console.log(csvData[row]['Location']);
     };
     
-    //create label for tooltip
-    var tooltip = d3.select('#section-1').append('div')
-                    .attr('class', 'tooltip')
-                    .style('opacity', 0);
-    
     vis.selectAll('.lines')
         .on('mouseover', function(){
             console.log(this.id);
-            d3.select('#' + (this.id).toString())
+            d3.select('#' + (this.id) + '')
                 .style("stroke-width", "6");
             d3.select('.tooltip')
                 .style("opacity", 1)
@@ -1469,78 +1467,67 @@ function stateGraph(csvData){
             d3.select('.tooltip')      
                 .style("opacity", 0);
         });
+    */    
     
-    $(".dropdown select").on("change", function(g) {
-        console.log('plese register');
-    })
+    updateStateGraph(lines, csvData, title);
+    /*
+    $('#total-events').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#avalanches').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#blizzards').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#droughts').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#excessive-heat').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#extreme-cold').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#tornado').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    $('#wildfire').on('click', function(){
+        updateStateGraph(vis, activeField, csvData, title, x, y);
+    });
+    */
+}
 
-    //create array out of only desired values and use them with the y axis generator
-    d3.selectAll(".dropdown-element a")
-        .on("change", function(e){//e is undefined right now... figure out why
-            console.log(e.target.value);
-            console.log('hello');
-            updateStateGraph(vis, e.target.value, csvData, title);
-    })
-
-};
-
-function updateStateGraph(vis, val, csvData, title){
-    console.log(val, csvData[0]);
-    var graphAttr;
-    
-    if (val == 'Avalanche'){
-        graphAttr = 'Avalanche';
-    }
-    else if (val == 'Blizzard'){
-        graphAttr = 'Blizzard'
-    }
-    else if (val == 'Drought'){
-        graphAttr = 'Drought'
-    }
-    else if (val == 'Excessive Heat'){
-        graphAttr = 'Excessive_Heat'
-    }
-    else if (val == 'Extreme Cold'){
-        graphAttr = 'Extreme_Cold'
-    }
-    else if (val == 'Tornado'){
-        graphAttr = 'Tornado'
-    }
-    else if (val == 'Wildfire'){
-        graphAttr = 'Wildfire'
-    }
-    else {
-        graphAttr = 'Total_Events'
-    }
+function updateStateGraph(lines, csvData, title){
+    console.log(activeField);
     
     //title
     var title = d3.select('#section-1')
-        .html('<br>' + val + ' By State</br>2000-2016')
+        .html('<br>' + activeField + ' By State</br>2000-2016')
         .style('font-family', 'Helvetica, sans-serif')
         .style('text-align', 'center')
         .style('font-weight', 'bold');    
     
     for (var row = 0; row < csvData.length; row++){
-    vis.append('polyline')
+    lines
         .attr('points', (x(2000)).toString() + ',' + (y(csvData[row]['Total_Events_2000'])).toString() + ',' +
-        (x(2001)).toString() + ',' + (y(csvData[row][graphAttr + '_2001'])).toString() + ',' +
-        (x(2002)).toString() + ',' + (y(csvData[row][graphAttr + '_2002'])).toString() + ',' +
-        (x(2003)).toString() + ',' + (y(csvData[row][graphAttr + '_2003'])).toString() + ',' +
-        (x(2004)).toString() + ',' + (y(csvData[row][graphAttr + '_2004'])).toString() + ',' +
-        (x(2005)).toString() + ',' + (y(csvData[row][graphAttr + '_2005'])).toString() + ',' +
-        (x(2006)).toString() + ',' + (y(csvData[row][graphAttr + '_2006'])).toString() + ',' +
-        (x(2007)).toString() + ',' + (y(csvData[row][graphAttr + '_2007'])).toString() + ',' +
-        (x(2008)).toString() + ',' + (y(csvData[row][graphAttr + '_2008'])).toString() + ',' +
-        (x(2009)).toString() + ',' + (y(csvData[row][graphAttr + '_2009'])).toString() + ',' +
-        (x(2010)).toString() + ',' + (y(csvData[row][graphAttr + '_2010'])).toString() + ',' +
-        (x(2011)).toString() + ',' + (y(csvData[row][graphAttr + '_2011'])).toString() + ',' +
-        (x(2012)).toString() + ',' + (y(csvData[row][graphAttr + '_2012'])).toString() + ',' +
-        (x(2013)).toString() + ',' + (y(csvData[row][graphAttr + '_2013'])).toString() + ',' +
-        (x(2014)).toString() + ',' + (y(csvData[row][graphAttr + '_2014'])).toString() + ',' +
-        (x(2015)).toString() + ',' + (y(csvData[row][graphAttr + '_2015'])).toString() + ',' +
-        (x(2016)).toString() + ',' + (y(csvData[row][graphAttr + '_2016'])).toString())
+        (x(2001)).toString() + ',' + (y(csvData[row][activeField + '_2001'])).toString() + ',' +
+        (x(2002)).toString() + ',' + (y(csvData[row][activeField + '_2002'])).toString() + ',' +
+        (x(2003)).toString() + ',' + (y(csvData[row][activeField + '_2003'])).toString() + ',' +
+        (x(2004)).toString() + ',' + (y(csvData[row][activeField + '_2004'])).toString() + ',' +
+        (x(2005)).toString() + ',' + (y(csvData[row][activeField + '_2005'])).toString() + ',' +
+        (x(2006)).toString() + ',' + (y(csvData[row][activeField + '_2006'])).toString() + ',' +
+        (x(2007)).toString() + ',' + (y(csvData[row][activeField + '_2007'])).toString() + ',' +
+        (x(2008)).toString() + ',' + (y(csvData[row][activeField + '_2008'])).toString() + ',' +
+        (x(2009)).toString() + ',' + (y(csvData[row][activeField + '_2009'])).toString() + ',' +
+        (x(2010)).toString() + ',' + (y(csvData[row][activeField + '_2010'])).toString() + ',' +
+        (x(2011)).toString() + ',' + (y(csvData[row][activeField + '_2011'])).toString() + ',' +
+        (x(2012)).toString() + ',' + (y(csvData[row][activeField + '_2012'])).toString() + ',' +
+        (x(2013)).toString() + ',' + (y(csvData[row][activeField + '_2013'])).toString() + ',' +
+        (x(2014)).toString() + ',' + (y(csvData[row][activeField + '_2014'])).toString() + ',' +
+        (x(2015)).toString() + ',' + (y(csvData[row][activeField + '_2015'])).toString() + ',' +
+        (x(2016)).toString() + ',' + (y(csvData[row][activeField + '_2016'])).toString())
         .attr('class', 'lines')
-        .attr('transform', 'translate(28,6)')
         .attr('id', function(){
             return csvData[row]['Location']
         })
@@ -1567,6 +1554,7 @@ function updateStateGraph(vis, val, csvData, title){
         console.log(csvData[row]['Location']);
     }; 
 }
+
 
 
 $(document).ready(initialize);
